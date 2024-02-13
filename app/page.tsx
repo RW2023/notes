74263c9e45eea6278fa5fe520c15a6367607e5bd
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-02-12 22:40:27 -0500

    remove main content and create and import component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import DeployButton from "../components/DeployButton";
-import AuthButton from "../components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/SignUpUserSteps";
-import Header from "@/components/Header";
+import { createClient } from '../utils/supabase/server';
+import AuthButton from '../components/AuthButton';
+import Landing from '@/components/ui/Landing';
 
 export default async function Index() {
   const canInitSupabaseClient = () => {
@@ -23,20 +20,15 @@ export default async function Index() {
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-       
           {isSupabaseConnected && <AuthButton />}
         </div>
       </nav>
 
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        
-        <main className="flex-1 flex flex-col gap-6">
-          {/* <h2 className="font-bold text-4xl mb-4">Next steps</h2> */}
-          {/* {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />} */}
-        </main>
-      </div>
+      {/* <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3"> */}
 
-     
+      <main className="flex-1 flex flex-col gap-6 justify-between">
+<Landing />
+      </main>
     </div>
   );
 }
