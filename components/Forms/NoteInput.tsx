af572485b4c47bc9c2c0b711cf6670eca206b599
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-02-13 19:30:48 -0500

    Add SubHeading component and update styles in NoteInput

--- components/Forms/NoteInput.tsx
+++ components/Forms/NoteInput.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { FC, useState } from 'react';
+import SubHeading from '../ui/SubHeading';
 
 interface Props {
     // Remove the props property
@@ -18,17 +19,18 @@ const NoteInput: FC<Props> = () => {
     };
 
     return (
-      <div className="flex flex-col justify-center items-center min-h-screen">
+      <div className="flex flex-col justify-center items-center">
+        <SubHeading title="Add a Note" />
         <form className="w-full max-w-xs">
           <div className="mb-4">
             <label
-              className="block text-gray-700 text-sm font-bold mb-2"
+              className="block text-headline text-sm font-bold mb-2"
               htmlFor="note-title"
             >
               Note Title:
             </label>
             <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-headline leading-tight focus:outline-none focus:shadow-outline"
               id="note-title"
               type="text"
               placeholder="Your note title"
@@ -38,18 +40,26 @@ const NoteInput: FC<Props> = () => {
           </div>
           <div className="mb-6">
             <label
-              className="block text-gray-700 text-sm font-bold mb-2"
+              className="block text-headline text-sm font-bold mb-2"
               htmlFor="note-text"
             >
               Note Text:
             </label>
             <textarea
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-headline leading-tight focus:outline-none focus:shadow-outline h-48 "
               id="note-text"
               placeholder="Your note text"
               value={text}
               onChange={handleTextChange}
             ></textarea>
+            <div className="flex-1 flex-col justify-center items-center">
+              <button
+                className="bg-green-700 hover:bg-success text-white font-bold py-2 px-4 rounded"
+                type="submit"
+              >
+                Add Note
+              </button>
+            </div>
           </div>
         </form>
       </div>
