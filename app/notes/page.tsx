e30b9d903e77a9d26560d9d1821897b3cc79b82e
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-02-13 19:30:17 -0500

    Add NoteList component to notes page

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,11 +1,19 @@
 import NoteInput from '@/components/Forms/NoteInput';
+import NoteList from '@/components/ui/NoteList';
 import { FC } from 'react';
 
 interface Props {}
 
 const page: FC<Props> = (props): JSX.Element => {
   return (
-  <NoteInput />
+    <div className="flex flex-row">
+      <div className="flex-1 p-3">
+        <NoteInput />
+      </div>
+      <div className="flex-1 p-3">
+        <NoteList />
+      </div>
+    </div>
   );
 };
 
