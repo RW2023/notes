4cd9755a1d46c92505e88503cd34649e0d7f6cd5
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-02-26 00:05:14 -0500

    Update UI layout in Landing component

--- components/ui/Landing.tsx
+++ components/ui/Landing.tsx
@@ -6,8 +6,16 @@ interface Props {}
 
 const Landing: FC<Props> = (props): JSX.Element => {
   return (
-    <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-3 bg-base-300 p-4 rounded-md drop-shadow-md">
-      <div className="w-full border-2 rounded-md p-2 flex flex-col justify-center items-center">
+    <div className="container grid grid-cols-1 lg:grid-cols-3 gap-4 bg-base-300 p-4 rounded-md drop-shadow-md">
+      <div className="col-span-1 lg:col-span-2 w-full border-2 rounded-md p-2 flex flex-col justify-center items-center">
+        <div className="title p-4 border border-1 rounded-md bg-black w-full">
+          <h2 className="text-3xl text-gray-300 text-center">Notes</h2>
+        </div>
+        <div className="content h-full m-2 overflow-auto">
+          <NoteList />
+        </div>
+      </div>
+      <div className="col-span-1 lg:col-span-1 w-full border-2 rounded-md p-2 flex flex-col justify-center items-center">
         <div className="title p-4 border border-1 rounded-md bg-black w-full">
           <h2 className="text-3xl text-headline text-center">Enter Notes</h2>
         </div>
@@ -15,15 +23,6 @@ const Landing: FC<Props> = (props): JSX.Element => {
           <NoteInput />
         </div>
       </div>
-      <div className="w-full border-2 rounded-md p-2 flex flex-col justify-center items-center">
-        <div className="title p-4 border border-1 rounded-md bg-black w-full">
-          <h2 className="text-3xl text-gray-300 text-center">Notes</h2>
-        </div>
-        <div className="content h-full m-2">
-          <NoteList />
-        </div>
-      </div>
-      <div></div>
     </div>
   );
 };
