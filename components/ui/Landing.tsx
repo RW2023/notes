6920c87cb5bf48c20489eb7a63c092abb76f6739
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-02-25 21:00:51 -0500

    Add NoteInput and NoteList components to Landing page

--- components/ui/Landing.tsx
+++ components/ui/Landing.tsx
@@ -1,4 +1,6 @@
 import { FC } from 'react';
+import NoteInput from '../Forms/NoteInput';
+import NoteList from './NoteList';
 
 interface Props {}
 
@@ -7,33 +9,18 @@ const Landing: FC<Props> = (props): JSX.Element => {
     <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-3 bg-base-300 p-4 rounded-md drop-shadow-md">
       <div className="w-full border-2 rounded-md p-2 flex flex-col justify-center items-center">
         <div className="title p-4 border border-1 rounded-md bg-black w-full">
-          <h2 className="text-3xl text-headline text-center">div 1</h2>
+          <h2 className="text-3xl text-headline text-center">Enter Notes</h2>
         </div>
         <div className="content h-full m-2">
-          <p>Content for the card body</p>
-          <img
-            className="rounded-md bg-black p-2 border-2 border-headline"
-            width={300}
-            height={300}
-            src="https://source.unsplash.com/random"
-            alt="Unsplash Image"
-          />
+          <NoteInput />
         </div>
-        <div className="content h-full"></div>
       </div>
       <div className="w-full border-2 rounded-md p-2 flex flex-col justify-center items-center">
         <div className="title p-4 border border-1 rounded-md bg-black w-full">
-          <h2 className="text-3xl text-gray-300 text-center">div 2</h2>
+          <h2 className="text-3xl text-gray-300 text-center">Notes</h2>
         </div>
         <div className="content h-full m-2">
-          <p>Content for the card body</p>
-          <img
-            className="rounded-md bg-black p-2 border-2 border-white"
-            width={300}
-            height={300}
-            src="https://source.unsplash.com/random"
-            alt="Unsplash Image"
-          />
+          <NoteList />
         </div>
       </div>
       <div></div>
