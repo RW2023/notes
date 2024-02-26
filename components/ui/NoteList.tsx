2e966063e5957707c85d0bbe1d12c5db4b217942
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-02-26 08:36:12 -0500

    Refactor NoteList component and add loading indicator

--- components/ui/NoteList.tsx
+++ components/ui/NoteList.tsx
@@ -1,7 +1,11 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { createClient } from '@supabase/supabase-js';
+import SubHeading from '@/components/ui/SubHeading';
+import Loading from './Loading';
+import LoadingInline from './LoadingInline';
+import UpdateNote from '../Forms/UpdateNote';
 
 type Note = {
   note_id: number;
@@ -10,43 +14,44 @@ type Note = {
   created_at: string;
 };
 
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
+);
+
 const NoteList = () => {
   const [notes, setNotes] = useState<Note[]>([]);
-
-  const supabase = createClientComponentClient();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchNotes = async () => {
-      let { data: fetchedNotes, error } = await supabase
-        .from('notes')
-        .select('*');
-
-      if (error) {
-        console.log('error', error);
-      } else {
-        // Check if fetchedNotes is not null before setting the state
-        if (fetchedNotes) {
-          setNotes(fetchedNotes);
-        }
-      }
-    };
-
     fetchNotes();
   }, []);
+
+  const fetchNotes = async () => {
+    let { data, error } = await supabase.from('notes').select('*');
+
+    if (error) {
+      console.error('Error fetching notes:', error.message);
+    } else {
+      setNotes(data || []);
+    }
+    setLoading(false);
+  };
+
+  if (loading) {
+    return <LoadingInline />;
+  }
 
   return (
     <div>
       {notes.map((note) => (
-        <div key={note.note_id} className="card bg-base-100 shadow-xl">
-          <div className="card-body">
-            <h3 className="card-title">{note.title}</h3>
-            <p>{note.note_text}</p>
-            <div className="card-actions justify-end">
-              <small>{new Date(note.created_at).toLocaleString()}</small>
-            </div>
-          </div>
+        <div key={note.note_id}>
+          <SubHeading title={note.title} />
+          <p>{note.note_text}</p>
+          <small>{new Date(note.created_at).toLocaleString()}</small>
         </div>
       ))}
+      <UpdateNote />
     </div>
   );
 };
