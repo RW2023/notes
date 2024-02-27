bcfba79a16db6625bc38d016b7ef1cc69e81b24d
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-02-27 12:00:14 -0500

    Add landing page component

---
+++ app/landing/page.tsx
@@ -0,0 +1,13 @@
+import Landing from '@/components/ui/Landing';
+import { FC } from 'react';
+
+interface Props {}
+
+const page: FC<Props> = (props): JSX.Element => {
+  return (
+    
+    <Landing />
+  );
+};
+
+export default page;
