51689aa80fd81495b0ea79825e31a4f688610071
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-02-27 14:01:23 -0500

    Refactor layout.tsx to use single quotes for imports

--- app/landing/layout.tsx
+++ app/landing/layout.tsx
@@ -1,21 +1,16 @@
-import AuthButton from "@/components/AuthButton";
-import Navbar from "@/components/ui/Navbar";
+import AuthButton from '@/components/AuthButton';
+import Navbar from '@/components/ui/Navbar';
 
-export default function Layout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    return (
-      <html lang="en" data-theme="business">
-        <body>
-          <Navbar />
-
-          <main className="min-h-screen flex flex-col items-center">
-            <AuthButton />
-            {children}
-          </main>
-        </body>
-      </html>
-    );
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" data-theme="business">
+      <body>
+        <Navbar />
+        <main className="min-h-screen flex flex-col items-center">
+          <AuthButton />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
 }
