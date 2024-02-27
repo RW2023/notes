64986ed23b1bd179c8d0157fcbc09be78532944e
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-02-27 08:37:11 -0500

    Add UpdateNoteForm component to Landing page

--- components/ui/Landing.tsx
+++ components/ui/Landing.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import NoteInput from '../Forms/NoteInput';
 import NoteList from './NoteList';
+import UpdateNoteForm from '../Forms/UpdateNote';
 
 interface Props {}
 
@@ -9,19 +10,20 @@ const Landing: FC<Props> = (props): JSX.Element => {
     <div className="container grid grid-cols-1 lg:grid-cols-3 gap-4 bg-base-300 p-4 rounded-md drop-shadow-md">
       <div className="col-span-1 lg:col-span-2 w-full border-2 rounded-md p-2 flex flex-col justify-center items-center">
         <div className="title p-4 border border-1 rounded-md bg-black w-full">
-          <h2 className="text-3xl text-gray-300 text-center">Notes</h2>
+          <h2 className="text-3xl text-headline text-center">Enter Notes</h2>
         </div>
         <div className="content h-full m-2 overflow-auto">
-          <NoteList />
+          <NoteInput />
         </div>
       </div>
       <div className="col-span-1 lg:col-span-1 w-full border-2 rounded-md p-2 flex flex-col justify-center items-center">
         <div className="title p-4 border border-1 rounded-md bg-black w-full">
-          <h2 className="text-3xl text-headline text-center">Enter Notes</h2>
+          <h2 className="text-3xl text-gray-300 text-center">Notes</h2>
         </div>
         <div className="content h-full m-2">
-          <NoteInput />
+          <NoteList />
         </div>
+        <UpdateNoteForm />
       </div>
     </div>
   );
