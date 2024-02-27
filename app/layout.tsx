9a8045b0e9f1ed74b2058e885eb6922877aaa5b1
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-02-27 11:00:55 -0500

    Update data-theme to "dark" in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" data-theme="black">
+    <html lang="en" data-theme="dark">
       <body>
         <main className="min-h-screen flex flex-col items-center">
           {children}
