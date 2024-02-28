e29d1b6f1d573a02d4665261cd6b3fe9fd32e6ea
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-02-27 19:41:01 -0500

    Refactor notes page layout and simplify it. pull in the new Notesgrid component

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,28 +1,19 @@
 import NoteInput from '@/components/Forms/NoteInput';
 import Heading from '@/components/ui/Heading';
 import NoteList from '@/components/ui/NoteList';
+import Layout from '@/components/layout';
 import { FC } from 'react';
+import Landing from '@/components/ui/NotesGridDisplay';
 
 interface Props {}
 
 const page: FC<Props> = (props): JSX.Element => {
   return (
-    <>
-      <div>
-        <Heading title="Notes" />
-      </div>
-      <div
-        className="flex flex-row border border-1 rounded-md"
-        data-theme="cmyk"
-      >
-        <div className="flex-1 p-3">
-          <NoteInput />
-        </div>
-        <div className="flex-1 p-3">
-          <NoteList />
-        </div>
-      </div>
-    </>
+    
+      <Layout>
+      <Landing />
+      </Layout>
+    
   );
 };
 
