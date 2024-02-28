5d6ce4e7af12598e2f49860deab910f096695a80
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-02-28 04:31:23 -0500

    Refactor NotesGridDisplay component

--- components/ui/NotesGridDisplay.tsx
+++ components/ui/NotesGridDisplay.tsx
@@ -7,23 +7,37 @@ interface Props {}
 
 const NoteGridDisplay: FC<Props> = (props): JSX.Element => {
   return (
-    <div className="container grid grid-cols-1 lg:grid-cols-3 gap-4 bg-base-300 p-4 rounded-md drop-shadow-md sm:w-full lg:w-2/3 m-auto">
-      <div className="col-span-1 lg:col-span-2 w-full border-2 rounded-md p-2 flex flex-col justify-center items-center">
-        <div className="title p-4 border border-1 rounded-md bg-black w-full">
-          <h2 className="text-3xl text-headline text-center">Enter Notes</h2>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-base-300">
+      <div className="container p-4 rounded-md drop-shadow-md max-w-6xl">
+        {' '}
+        {/* Adjust max-w as needed */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
+          {/* "Enter Notes" section */}
+          <div className="mb-4 flex flex-col">
+            <div className="p-4 bg-black rounded-md w-full mb-4">
+              <h2 className="text-3xl text-headline text-center">
+                Enter Notes
+              </h2>
+            </div>
+            <div className="flex-grow m-2 overflow-auto">
+              <NoteInput />
+            </div>
+          </div>
+
+          {/* "Notes" section */}
+          <div className="mb-4 flex flex-col">
+            <div className="p-4 bg-black rounded-md w-full mb-4">
+              <h2 className="text-3xl text-gray-300 text-center">Notes</h2>
+            </div>
+            <div className="flex-grow m-2 overflow-auto">
+              <NoteList />
+            </div>
+          </div>
         </div>
-        <div className="content h-full m-2 overflow-auto">
-          <NoteInput />
+        {/* "Update Note" form, full width below the two columns */}
+        <div className="mt-4 w-full mx-auto">
+          <UpdateNoteForm />
         </div>
-      </div>
-      <div className="col-span-1 lg:col-span-1 w-full border-2 rounded-md p-2 flex flex-col justify-center items-center">
-        <div className="title p-4 border border-1 rounded-md bg-black w-full">
-          <h2 className="text-3xl text-gray-300 text-center">Notes</h2>
-        </div>
-        <div className="content h-full m-2">
-          <NoteList />
-        </div>
-        <UpdateNoteForm />
       </div>
     </div>
   );
